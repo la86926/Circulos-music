083f8adcbcd185edfc09b4affe07a8c36effc9c6
la86926 <la86926@gmail.com>
2026-09-23 14:23:25 -0500

    Keep chord library independent from Circulos UI

--- chords-library.js
+++ chords-library.js
@@ -2,7 +2,6 @@
 'use strict';
 if(window.__circulosChordLibraryV10)return;
 window.__circulosChordLibraryV10=true;
-Promise.all([import('./circle-wheel.js?v=9'),import('./performance-mode.js?v=10')]).catch(()=>{});
 
 const DATA_URL='chords-data.json?v=1';
 const ROOT_PC={C:0,'B#':0,'C#':1,Db:1,D:2,'D#':3,Eb:3,E:4,Fb:4,'E#':5,F:5,'F#':6,Gb:6,G:7,'G#':8,Ab:8,A:9,'A#':10,Bb:10,B:11,Cb:11};
